275400c6bb5d8c9936a05fd4aa6ceb117ab9bcef
RobozPeter <peti.roboz@gmail.com>
2025-01-24 10:54:13 +0100

    feat(SCRUM-11):cleanup

--- src/scripts/uploadDataScript.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as csv from 'csv-parser';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-
-//Object.assign( gyakorlatColumnMapping);
