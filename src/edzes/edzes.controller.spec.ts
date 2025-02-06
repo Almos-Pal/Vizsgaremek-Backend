4f0835d9d5624745c65e5ee3ae675409c484b413
RobozPeter <peti.roboz@gmail.com>
2025-02-06 08:22:37 +0100

    feat(SCRUM-30):WIP

--- src/edzes/edzes.controller.spec.ts
+++ src/edzes/edzes.controller.spec.ts
@@ -1,46 +1,37 @@
+import { Edzes, PrismaPromise } from '@prisma/client';
 import { PrismaService } from '../prisma.service';
 import { EdzesController } from './edzes.controller';
 import { EdzesService } from './edzes.service';
 import { Test } from '@nestjs/testing';
 
 describe('EdzesController', () => {
+
   let edzesController: EdzesController;
   let edzesService: EdzesService;
-  let asd : any = [{ edzes_id: 1, edzes_neve: "string", datum: new Date("2024-11-12"), user_id: 1, ido: 12 }]
+  let asd: Edzes[]= [];
+   asd.push({edzes_id: 1, edzes_neve: "string", datum: new Date("2024-11-12"), user_id: 1, ido: 12});
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       controllers: [EdzesController],
-      providers: [EdzesService, PrismaService],
+      providers: [EdzesService,PrismaService],
     }).compile();
-
 
     edzesService = moduleRef.get(EdzesService);
     edzesController = moduleRef.get(EdzesController);
   });
 
-
   describe('findAll', () => {
-    it('should return an array of edzes', async () => {
-      const result: any = [{ edzes_id: 1, edzes_neve: "string", datum: new Date("2024-11-12"), user_id: 1, ido: 12 }];
-      jest.spyOn(edzesService, 'findAll').mockImplementation(() => result);
-      expect(await edzesController.findAll()).toBe(result);
-    });
     it('should return an array', () => {
-      jest.spyOn(edzesService, 'findAll').mockReturnValue(asd)
-      expect(edzesController.findAll()).toBe(asd);
+      jest.spyOn(edzesService, 'findAll').mockReturnValue( Promise.resolve(asd) as PrismaPromise<Edzes[]>);
+      expect(edzesController.findAll()).resolves.toEqual(asd);
+    });
+  });
+  describe('findOne', () => {
+    it('should return one element', () => {
+      jest.spyOn(edzesService, 'findOne').mockReturnValue( Promise.resolve(asd[0]) as PrismaPromise<Edzes>);
+      expect(edzesController.findOne(1)).resolves.toEqual(asd[0]);
     });
   });
   
-
-  /*
-  describe('findOne', () => {
-    it('should return an edzes', async () => {
-      const result: any ={ edzes_id: 1, edzes_neve: "string", datum: new Date("2024-11-12"), user_id: 1, ido: 12 };
-      jest.spyOn(edzesService, 'findOne').mockImplementation(() => result);
-
-      expect(await edzesController.findOne(@Param('1'),)).toBe(result);
-    });
-  });
-  */
 });
