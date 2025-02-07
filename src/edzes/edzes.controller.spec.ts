a9144744a839d765e3b5f3fc36bb42243630cc59
RobozPeter <peti.roboz@gmail.com>
2025-02-07 13:22:18 +0100

    test(SCRUM-30):edzes controller tests DONE

--- src/edzes/edzes.controller.spec.ts
+++ src/edzes/edzes.controller.spec.ts
@@ -1,15 +1,63 @@
-import { Edzes, PrismaPromise } from '@prisma/client';
 import { PrismaService } from '../prisma.service';
 import { EdzesController } from './edzes.controller';
 import { EdzesService } from './edzes.service';
 import { Test } from '@nestjs/testing';
 
+import { GetEdzesekQueryDto } from './dto/get-edzesek.dto';
+import { UpdateEdzesDto } from './dto/update-edzes.dto';
+import { CreateEdzesDto } from './dto/create-edzes.dto';
+
 describe('EdzesController', () => {
+  const updateMockData:UpdateEdzesDto = {
+    edzes_neve: "string;",
+    ido: 11000
+  }
+
+const edzesMockData: any = {
+  items: [{
+    gyakorlatok: [{
+        total_sets: 5,
+        gyakorlat: {
+            izomcsoportok: {
+                izomcsoport: {
+                    izomcsoport_id: 1,
+                    nev: "string;",
+                },
+            }  
+        }, 
+        szettek: [{
+            suly: 1,
+            ismetles: 1,
+            id: 1,
+        }],
+        edzes_id: 1,
+        gyakorlat_id: 1,
+    }],
+    edzes_id: 12,
+    edzes_neve: "string;",
+    datum: new Date("2024-03-20"),
+    ido: 11
+}],
+  meta: {
+    currentPage: 1,
+    itemsPerPage: 10,
+    totalItems: 1,
+    totalPages: 1
+  }
+}
+
+
+
+  const edzesdto: GetEdzesekQueryDto = {}
+  const createMockData: CreateEdzesDto = {
+    edzes_neve: "string;",
+    datum : "2025-01-01",
+    user_id:1,
+    ido:2
+  }
 
   let edzesController: EdzesController;
   let edzesService: EdzesService;
-  let edzesMockData: Edzes[]= [];
-  edzesMockData.push({edzes_id: 1, edzes_neve: "string", datum: new Date("2024-11-12"), user_id: 1, ido: 12});
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -23,15 +71,33 @@ describe('EdzesController', () => {
 
   describe('findAll', () => {
     it('should return an array', () => {
-      jest.spyOn(edzesService, 'findAll').mockResolvedValue( edzesMockData);
-      expect(edzesController.findAll()).resolves.toEqual(edzesMockData);
+      jest.spyOn(edzesService, 'findAll').mockResolvedValue(edzesMockData );
+      expect(edzesController.findAll(edzesdto)).resolves.toEqual(edzesMockData);
     });
   });
   describe('findOne', () => {
     it('should return one element', () => {
-      jest.spyOn(edzesService, 'findOne').mockResolvedValue(edzesMockData[0]);
-      expect(edzesController.findOne(1)).resolves.toEqual(edzesMockData[0]);
+      jest.spyOn(edzesService, 'findOne').mockResolvedValue(edzesMockData.items[0]);
+      expect(edzesController.findOne(1)).resolves.toEqual(edzesMockData.items[0]);
     });
   });
-  
+  const updatedMock = {updateMockData, ...edzesMockData }
+  describe('update', () => {
+    it('should update one element', () => {
+      jest.spyOn(edzesService, 'update').mockResolvedValue(updatedMock);
+      expect(edzesController.update(1,updateMockData)).resolves.toEqual(updatedMock);
+    });
+  });
+  describe('remove', () => {
+    it('should remove one element', () => {
+      jest.spyOn(edzesService, 'remove').mockResolvedValue(edzesMockData.items[0]);
+      expect(edzesController.remove(1)).resolves.toEqual(edzesMockData.items[0]);
+    });
+  });
+  describe('create', () => {
+    it('should create new element', () => {
+      jest.spyOn(edzesService, 'create').mockResolvedValue(edzesMockData.items[0]);
+      expect(edzesController.create(createMockData)).resolves.toEqual(edzesMockData.items[0]);
+    });
+  });
 });
